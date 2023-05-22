68000af01d52b039a06ba6195cbbd0126122ae22
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-05-22 20:59:07 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Stateful Counter</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
